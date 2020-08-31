0f6e3f224e816a67022dbb108585cca8ba4b6a66
Juan Manuel <59283605+juanmaMacGyverCode@users.noreply.github.com>
2020-08-31 23:33:19 +0100

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -3,7 +3,7 @@ from calculator import multiply
 
 class TestSomething(unittest.TestCase):
 	def test_multiply(self):
-		self.assertEqual(3, multiply(2,2))
+		self.assertEqual(4, multiply(2,2))
 
 if __name__ == '__main__':
 	unittest.main()
